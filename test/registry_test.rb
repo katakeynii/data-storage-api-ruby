9c3e29295b1d57d73b3b7e35a85c10971c31b942
Mohamed Camara GUEYE <katakeynii@gmail.com>
2021-04-21 08:52:29 +0000

    asserting registry instances

--- test/registry_test.rb
+++ test/registry_test.rb
@@ -2,15 +2,14 @@ require "minitest/autorun"
 require_relative '../src/router'
 require_relative '../src/router/route'
 require_relative '../src/router/registry'
-class RouteTest < Minitest::Test
 
-    def test_correct_route
-      @home_route = Router::Route.new "get", "/", "HomeController"
-      assert_instance_of Router::Route, @home_route
-      assert_equal @home_route.path, "/"
-      assert_raises RuntimeError do 
-         Router::Route.new "set", "/", "HomeController"
-      end
+class RegistryTest < Minitest::Test
+
+    def test_instances
+      registry = Router::Registry.new
+      registry.add "get", "/", "HomeController#index"
+      assert_instance_of Router::Registry, registry
+      assert_instance_of Router::Route, registry.routes.first
     end
   
 end
