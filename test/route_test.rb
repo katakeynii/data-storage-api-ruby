71cc3ed1589ed92faa7eb636d920b7dd1862d096
Mohamed Camara GUEYE <katakeynii@gmail.com>
2021-04-21 08:43:05 +0000

    updating test to add name setting

--- test/route_test.rb
+++ test/route_test.rb
@@ -4,12 +4,17 @@ require_relative '../src/router/route'
 class RouteTest < Minitest::Test
 
     def test_correct_route
-      @home_route = Router::Route.new "get", "/", "HomeController"
+      @home_route = Router::Route.new "get", "/", "HomeController#home"
       assert_instance_of Router::Route, @home_route
       assert_equal @home_route.path, "/"
       assert_raises RuntimeError do 
          Router::Route.new "set", "/", "HomeController"
       end
+    end
+
+    def test_route_name
+        @home_route = Router::Route.new "get", "/", "HomeController#index"
+        assert_equal @home_route.name, "get_home_controller_index" 
     end
   
 end
